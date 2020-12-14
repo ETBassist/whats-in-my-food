8825b78e6367bbfd342a5f2d75cb659856300826
Eugene Theriault <hybridbassist@gmail.com>
2020-12-14 08:47:48 -0800

    Create test for food service

---
+++ spec/services/food_service_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'Food Service' do
+  it 'can return searched-for foods' do
+    response = FoodService.search_for_food('sweet potatoes')
+
+    expect(response).to be_a(Hash)
+
+    expect(response).to have_key(:gtinUpc)
+    expect(response).to have_key(:description)
+    expect(response).to have_key(:brandOwner)
+    expect(response).to have_key(:ingredients)
+  end
+end
