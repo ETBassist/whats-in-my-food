6e2acd29ac48c52020139a09201220cac0af951d
Eugene Theriault <hybridbassist@gmail.com>
2020-12-14 08:18:47 -0800

    Create test for food search

---
+++ spec/features/foods/ingredient_search_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  describe 'if I visit / and search for sweet potatoes' do
+    it 'I should see a list of ten food' do
+      visit '/'
+      fill_in :q, with: 'sweet potatoes'
+
+      click_button('Search')
+      expect(current_path).to eq('/foods')
+
+      expect(page).to have_selector('.gtin/upc', count: 10)
+      expect(page).to have_selector('.description', count: 10)
+      expect(page).to have_selector('.brand-owner', count: 10)
+      expect(page).to have_selector('.ingredients', count: 10)
+      expect(page).to have_content('sweet potatoes')
+    end
+  end
+end
