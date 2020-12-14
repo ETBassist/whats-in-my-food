3ab73ca2453c743ef7df4949e3094de97f8f1965
Eugene Theriault <hybridbassist@gmail.com>
2020-12-14 08:57:07 -0800

    Create FoodService class
    Also edits test to check for key in first result only and to expect an
    Array instead of a Hash

---
+++ app/services/food_service.rb
@@ -0,0 +1,22 @@
+class FoodService
+  def self.search_for_food(keyword)
+    get_json("/fdc/v1/foods/search?query=#{keyword}")[:foods]
+  end
+
+  module Connectable
+    private
+
+    def get_json(uri)
+      response = conn.get(uri)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def conn
+      Faraday.new(url: 'https://api.nal.usda.gov') do |faraday|
+        faraday.params['api_key'] = ENV['FOOD_API_KEY']
+      end
+    end
+  end
+
+  extend Connectable
+end
--- spec/services/food_service_spec.rb
+++ spec/services/food_service_spec.rb
@@ -4,11 +4,11 @@ describe 'Food Service' do
   it 'can return searched-for foods' do
     response = FoodService.search_for_food('sweet potatoes')
 
-    expect(response).to be_a(Hash)
+    expect(response).to be_a(Array)
 
-    expect(response).to have_key(:gtinUpc)
-    expect(response).to have_key(:description)
-    expect(response).to have_key(:brandOwner)
-    expect(response).to have_key(:ingredients)
+    expect(response.first).to have_key(:gtinUpc)
+    expect(response.first).to have_key(:description)
+    expect(response.first).to have_key(:brandOwner)
+    expect(response.first).to have_key(:ingredients)
   end
 end
