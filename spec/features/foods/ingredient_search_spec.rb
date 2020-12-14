96ebb5d70ee16c3d8fdcf5dbafea774bb4ea18fc
Eugene Theriault <hybridbassist@gmail.com>
2020-12-14 09:33:28 -0800

    Add handling for no foods found in search

--- spec/features/foods/ingredient_search_spec.rb
+++ spec/features/foods/ingredient_search_spec.rb
@@ -9,11 +9,21 @@ describe 'As a user' do
       click_button('Search')
       expect(current_path).to eq('/foods')
 
+      expect(page).to have_content('Total Hits: 39244')
       expect(page).to have_selector('.gtin-upc', count: 10)
       expect(page).to have_selector('.description', count: 10)
       expect(page).to have_selector('.brand-owner', count: 10)
       expect(page).to have_selector('.ingredients', count: 10)
       expect(page).to have_content('SWEET POTATOES')
+    end
+
+    it "If I search for nonsense I see that no results were found" do
+      visit '/'
+      fill_in :q, with: 'awholelottagibberish'
+      click_button('Search')
+
+      expect(page).to have_content('Total Hits: 0')
+      expect(page).to have_content('No Foods Found')
     end
   end
 end
